6e01d2f87d16f54d6189a03525d76cfb4ea29d8e
Sebastian Ramirez <srguerra27@gmail.com>
2020-06-18 01:27:27 -0300

    Elimine dos carpetas vacias llamadas programa/Progrma y modifique la barra de navegacion de menu.php

--- menu.php
+++ menu.php
@@ -3,13 +3,17 @@
 <head>
 	<title>PROGRAMA</title>
 	<?php
-	require_once "vistas/dependencias.php"
+	require_once "vistas/dependencias.php";
 	?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 	<link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.min.css">
 	<script src="librerias/jquery-3.5.1.min.js"></script>
 	
 </head>
 <body>
+	<!--
 	<div class="row no-gutters">
 		<div class="col-6 col-md-4">
 			<div class="jumbotron">
@@ -19,35 +23,20 @@
 				<p></p>
 				<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 			</div>
-
-		</div>	
-		<div class="col-sm-6 col-md-8">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="#">Navbar</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Features</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Pricing</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-						</li>
-					</ul>
-				</div>
-			</nav>	
-
-
 		</div>
-
+		-->	
+		<div>
+			<nav class="nav nav-light bg-light  justify-content-end">
+				<ul class="nav">
+					<li class="nav-item">
+						<a class="nav-link active" href="#">Login</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div>
+			<h1>div de menu de acciones</h1>
+		</div>
 	</div>
 </body>
 </html>
